b60caffb196358e621502aa49d18cc3fd03210c0
KelseyGibbs <gibbs.kelseylee@gmail.com>
2019-06-26 18:52:46 -0500

    added basic auth with the help of several tutorials

--- server.js
+++ server.js
@@ -1,23 +1,37 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const logger = require("morgan");
+const users = require("./routes/api/users");
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
 // Define API routes here
+app.use("/api/users", users);
 
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// Connect to database. Either local or MLab if deployed
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/WholeBaker";
+
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true
 });
+
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
